91e309a6b7edae330b02dee3aeebb0ff9cdde7ae
Mario Novella <mario.novella@gmail.com>
2023-08-17 19:29:03 +0200

    added place, button ando modal components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import {
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <MainNavigation />
       <Routes>
         <Route path="/" element={<Users/>}/>
+        <Route path="/:userId/places" element={<UserPlaces />}/>
         <Route path="/places/new" element={<NewPlace />} />
         <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
