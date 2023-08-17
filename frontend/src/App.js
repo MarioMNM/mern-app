3c8440a8e4a3ffd8a537eaa8e9eccf291bef0b26
Novella Murillo <mario.novella-murillo@capgemini.com>
2023-08-17 08:45:16 +0200

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className>
+      <header className="App-header">
+        React App!
+      </header>
+    </div>
+  );
+}
+
+export default App;
