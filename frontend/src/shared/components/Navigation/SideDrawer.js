1167e23a02b759639edba3975a196e2d7cc89ea6
Mario Novella <mario.novella@gmail.com>
2023-08-18 11:47:28 +0200

    added map with google maps api

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
+  const nodeRef = useRef(null);
+
   const content = (
     <CSSTransition
+      nodeRef={nodeRef}
       in={props.show}
       timeout={200}
       classNames="slide-in-left"
--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
@@ -29,15 +29,19 @@ const ModalOverlay = (props) => {
 };
 
 const Modal = (props) => {
+  const nodeRef = useRef(null);
+
   return (
     <React.Fragment>
       {props.show && <Backdrop onClick={props.onCancel} />}
+
       <CSSTransition
         in={props.show}
         mountOnEnter
         unmountOnExit
         timeout={200}
         classNames="modal"
+        nodeRef={nodeRef}
       >
         <ModalOverlay {...props} />
       </CSSTransition>
