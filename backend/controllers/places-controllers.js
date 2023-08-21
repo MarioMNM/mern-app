c231f15487916669da3a7079c0a20324179ca422
Mario Novella <mario.novella@gmail.com>
2023-08-21 10:58:36 +0200

    added get places routes

---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,48 @@
+const HttpError = require("../models/http-errors");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const userPlaces = DUMMY_PLACES.find((u) => {
+    return u.creator === userId;
+  });
+
+  if (!userPlaces) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ userPlaces });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
