1c59dc73fd2382294b44da987f435b2990c9f768
Mario Novella <mario.novella@gmail.com>
2023-08-21 15:04:55 +0200

    added func to get coords from google maps api

---
+++ backend/util/location.js
@@ -0,0 +1,26 @@
+const HttpError = require("../models/http-errors");
+const axios = require("axios");
+
+const API_KEY = "AIzaSyDmiPVXuQYNLow_X66yYO6-fLlKl6IcTFs";
+
+async function getCoordsForAddress(address) {
+  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
+    address
+  )}&inputtype=textquery&fields=geometry&key=${API_KEY}`;
+ 
+  const response = await axios.get(url);
+ 
+  const data = response.data;
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+    throw error;
+  }
+  const coordinates = data.candidates[0].geometry.location;
+ 
+  return coordinates;
+};
+
+module.exports = getCoordsForAddress;
