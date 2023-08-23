256c4995bcc145955aa4a6331fa59c8888f1d7dc
Mario Novella <mario.novella@gmail.com>
2023-08-23 21:03:50 +0200

    added request for updating places

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -6,35 +6,6 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-// const DUMMY_PLACES = [
-//   {
-//     id: "p1",
-//     title: "Empire State Building",
-//     description: "One of the most famous sky scrapers in the world!",
-//     imageUrl:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-//     address: "20 W 34th St, New York, NY 10001",
-//     location: {
-//       lat: 40.7484405,
-//       lng: -73.9878584,
-//     },
-//     creator: "u1",
-//   },
-//   {
-//     id: "p2",
-//     title: "Emp. State Building",
-//     description: "One of the most famous sky scrapers in the world!",
-//     imageUrl:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-//     address: "20 W 34th St, New York, NY 10001",
-//     location: {
-//       lat: 40.7484405,
-//       lng: -73.9878584,
-//     },
-//     creator: "u2",
-//   },
-// ];
-
 const UserPlaces = () => {
   const userId = useParams().userId;
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -51,7 +22,7 @@ const UserPlaces = () => {
       } catch (err) {}
     };
     fetcPlaces();
-  }, [sendRequest]);
+  }, [sendRequest, userId]);
 
   return (
     <React.Fragment>
