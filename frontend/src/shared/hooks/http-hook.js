3449052180bcb79e1bda91323051df02e1199268
Mario Novella <mario.novella@gmail.com>
2023-08-23 19:51:23 +0200

    refactored requests with http-hook

---
+++ frontend/src/shared/hooks/http-hook.js
@@ -0,0 +1,58 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequest = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrll = new AbortController();
+      activeHttpRequest.current.push(httpAbortCtrll);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrll.signal,
+        });
+
+        const responseData = await response.json();
+
+        activeHttpRequest.current = activeHttpRequest.current.filter(
+          (reqCtrll) => reqCtrll !== httpAbortCtrll
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(err.message);
+          throw err;
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequest.current.forEach((abortCtrll) => abortCtrll.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
